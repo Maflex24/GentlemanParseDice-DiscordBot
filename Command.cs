a591e1bec747756527533113796efa0db131c9dc
Maflex24 <maflex24@gmail.com>
2022-05-08 14:12:48 +0200

    Extension Command class Created DataHandler

--- Command.cs
+++ Command.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 
 namespace GentelmanParserDiscordBot
 {
     public static class Command
     {
+        static Dictionary<string, List<string>> CommandsAndOutputMessages = new Dictionary<string, List<string>>();
+
         public static bool IsCommandValid(string command)
         {
-            return true; // todo implementation
+            if (CommandsAndOutputMessages.Keys.Contains(command))
+                return true;
+
+            return false;
         }
 
-        public static void ExecuteCommand(string command)
+        public static void ExecuteCommand(string command, SocketMessage message)
         {
+            int outputIndex = 0;
+            int outputElements = CommandsAndOutputMessages[command].Count;
 
+            if (outputElements > 1)
+            {
+                Random random = new Random();
+                outputIndex = random.Next(0, outputElements);
+            }
+
+            message.Channel.SendMessageAsync(CommandsAndOutputMessages[command][outputIndex]);
         }
     }
 }
