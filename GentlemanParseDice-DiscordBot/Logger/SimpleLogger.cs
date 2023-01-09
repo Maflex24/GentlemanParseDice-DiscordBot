a61f8bb184b118bae37fbcf11a1f1a26d0deefe9
Maflex24 <49068385+Maflex24@users.noreply.github.com>
2023-01-09 23:53:48 +0100

    Added separate rolls logs, with possibility open in spreedsheet

--- GentlemanParseDice-DiscordBot/Logger/SimpleLogger.cs
+++ GentlemanParseDice-DiscordBot/Logger/SimpleLogger.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace GentelmanParserDiscordBot.Logger
@@ -26,7 +27,16 @@ namespace GentelmanParserDiscordBot.Logger
             var rolledOrCommandInfoPadLength = 140;
 
             if (logtype is LogType.Roll)
-                rolledOrCommandInfo = $"For {log.Command} rolled: {log.RollData.Sum}";
+            {
+                var rolls = new StringBuilder();
+                foreach (var roll in log.RollData.Rolls)
+                {
+                    rolls.Append(roll + " ");
+                }
+
+                rolledOrCommandInfo = $"For {log.Command} rolled: {log.RollData.Sum} | {log.RollData.DiceType}: {rolls.ToString()}";
+                logRolls(log);
+            }
             else
             {
                 rolledOrCommandInfo = log.Command.PadRight(10) + separator;
@@ -54,6 +64,34 @@ namespace GentelmanParserDiscordBot.Logger
             File.AppendAllText(logFile, "\n" + builder.ToString());
 
             Console.WriteLine(builder.ToString());
+        }
+
+        private static void logRolls(Log log)
+        {
+            var outBuilder = new StringBuilder();
+            var separator = " | ";
+
+            foreach (var roll in log.RollData.Rolls)
+            {
+                StringBuilder insideBuilder = new StringBuilder();
+
+                insideBuilder.Append(DateTime.Now.ToString().PadRight(20));
+                insideBuilder.Append(separator);
+
+                insideBuilder.Append(log.Author.PadRight(15));
+                insideBuilder.Append(separator);
+
+                insideBuilder.Append($"d{log.RollData.DiceType}".PadRight(5));
+                insideBuilder.Append(separator);
+
+                insideBuilder.Append(roll);
+                insideBuilder.Append(separator + "\n");
+
+                outBuilder.Append(insideBuilder.ToString());
+            }
+
+            var logFile = DevelopmentInfo.GetRollsFilePath();
+            File.AppendAllText(logFile, outBuilder.ToString());
         }
     }
 }
